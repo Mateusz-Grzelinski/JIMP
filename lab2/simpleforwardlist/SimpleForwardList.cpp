03b7a9323ccf48e006b9e910f1b79e0cb280bbe5
Mateusz Grzeliński <czubaka111@gmail.com>
2017-03-12 14:11:46 +0100

    Simple Forward List przechodzi test 1

--- lab2/simpleforwardlist/SimpleForwardList.cpp
+++ lab2/simpleforwardlist/SimpleForwardList.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "SimpleForwardList.h"
+
+
+ForwardList *CreateNode(int value){
+    ForwardList *node= new ForwardList;
+    (*node).value=value;
+    (*node).next=nullptr;
+    return node;
+}
+
+
+void DestroyList(ForwardList *list){
+    ForwardList *tmp;
+    while(list!=nullptr){
+        tmp=list;
+        list=list->next;
+        delete tmp;
+    }
+}
