99bf39d7d9a55fdaa993215b13d5ce1777edd6c9
Mateusz Grzeliński <czubaka111@gmail.com>
2017-03-16 09:05:44 +0100

    przygotowania do ccounter

---
+++ lab3/ccounter/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by mat on 16.03.17.
+//
+
+#include <iostream>
+#include "CCounter.h"
+
+using namespace std;
+int main(){
+    std::unique_ptr<Counter> nowyObiekt = ccounter::Init();
+//    cout<<nowyObiekt;
+    return 0;
+}
