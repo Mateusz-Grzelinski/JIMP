b450772a035efe819b9a86432360a75b9b0a4085
Mateusz Grzeliński <czubaka111@gmail.com>
2017-04-09 13:49:48 +0200

    travis

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CMAKEFlAGS=-DCMAKE_BUILD_TYPE=Debug
+CMAKEFlAGS="-DCMAKE_BUILD_TYPE=Debug"
 
 #opcja k powoduje że make nie zamyka się po napotkaniu błędu
-MAKEFLAGS=--silent -k
+MAKEFLAGS="--silent -k"
 
 
 cmake . $CMAKEFLAGS
@@ -18,14 +18,14 @@ for testfolder in lab*; do
 
 	#jeśli istnieje, odpal lab?_all_tests
 	if [ -f *_all_tests ]; then
-		#./*_all_tests
+		/*_all_tests
 		echo *all_tests
 	else
 	#jeśli nie ma, odpalaj testy pojedyńczo:
 		for singletest in lab*; do
 			#ten test buduje się, ale się loopuje. Co z tym fantem zrobić?
 			if [ ! "$singletest" = "lab4_simple_template_engine_injection_tests" ]; then
-			#./$singletest
+			./$singletest
 			echo $singletest
 			fi;
 		done;
