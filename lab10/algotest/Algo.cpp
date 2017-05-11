c0f25eb665174bfb1f3b4d199ee55d62eb71c11f
Mateusz Grzeliński <czubaka111@gmail.com>
2017-05-11 10:52:30 +0200

    Algo- zajęcia

---
+++ lab10/algotest/Algo.cpp
@@ -0,0 +1,95 @@
+//
+// Created by mat on 11.05.17.
+//
+
+#include "Algo.h"
+
+namespace algo {
+    std::set<std::string> Keys(const std::map<std::string, int> &m) {
+        return std::set<std::string>();
+    }
+
+    std::set<std::string> Values(const std::map<std::string, int> &m) {
+        return std::set<std::string>();
+    }
+
+    std::map<std::string, int> DivisableBy(const std::map<std::string, int> &m, int divisor) {
+        return std::map<std::string, int>();
+    }
+
+    void SortInPlace(std::vector<int> *v) {
+
+    }
+
+    std::vector<int> Sort(const std::vector<int> &v) {
+        return std::vector<int>();
+    }
+
+    void SortByFirstInPlace(std::vector<std::pair<int, int>> *v) {
+
+    }
+
+    void SortBySecondInPlace(std::vector<std::pair<int, int>> *v) {
+
+    }
+
+    void SortByThirdInPlace(std::vector<std::tuple<int, int, int>> *v) {
+
+    }
+
+    std::vector<std::string> MapToString(const std::vector<double> &v) {
+        std::vector<std::string> vectmp;
+//        std::for_each(v.begin(), v.end(), [&vectmp](const double v_it){ vectmp.push_back(std::to_string(v_it)); });
+        std::transform(v.begin(), v.end(), std::back_inserter(vectmp),  [](const double &v_it){ return std::to_string(v_it); });
+        return vectmp;
+    }
+
+    std::string Join(const std::string &joiner, const std::vector<double> &v) {
+        return std::__cxx11::string();
+    }
+
+    int Sum(const std::vector<int> &v) {
+        return 0;
+    }
+
+    int Product(const std::vector<int> &v) {
+        return 0;
+    }
+
+    bool Contains(const std::vector<int> &v, int element) {
+        return std::any_of(v.begin(), v.end(), [element](const int &v_it){ return v_it==element; } );
+        //[ element zewnętrzny ](iterator)
+    }
+
+    bool ContainsKey(const std::map<std::string, int> &v, const std::string &key) {
+        return false;
+    }
+
+    bool ContainsValue(const std::map<std::string, int> &v, int value) {
+        return false;
+    }
+
+    std::vector<std::string> RemoveDuplicates(const std::vector<std::string> &v) {
+        return std::vector<std::string>();
+    }
+
+    void RemoveDuplicatesInPlace(std::vector<std::string> *v) {
+
+    }
+
+    void InitializeWith(int initial_value, std::vector<int> *v) {
+        std::fill(v->begin(), v->end(), initial_value);
+    }
+
+    std::vector<int> InitializedVectorOfLength(int length, int initial_value) {
+        return std::vector<int> (length,initial_value);
+    }
+
+    void CopyInto(const std::vector<int> &v, int n_elements, std::vector<int> *out) {
+        std::copy(v.begin(), v.begin()+n_elements, std::back_inserter(*out));
+    }
+
+    int HowManyShortStrings(const std::vector<std::string> &v, int inclusive_short_length) {
+        return 0;
+    }
+}
